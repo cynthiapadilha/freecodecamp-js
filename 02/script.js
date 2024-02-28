44aca541c3509b951f69ff656286c87cf27a4b4e
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-02-28 19:25:01 -0300

    add functions

--- 02/script.js
+++ 02/script.js
@@ -9,13 +9,19 @@ let isError = false;
 /*transformando string em number*/
 
 function cleanInputString(str) {
-  const strArray = str.split('');
+  /*const strArray = str.split('');
   const cleanStrArray = [];
-/* Embora o loop pela string funcione, a criação de uma nova matriz é ineficiente para a memória e o desempenho do tempo de execução. Em vez disso vamos usar regex.
+ Embora o loop pela string funcione, a criação de uma nova matriz é ineficiente para a memória e o desempenho do tempo de execução. Em vez disso vamos usar regex.
 
   for (let i = 0; i < strArray.length; i++) {
     if (!["+", "-", " "].includes(strArray[i])) {
       cleanStrArray.push(strArray[i])
     }
   }*/
+  const regex = /[+-\s]/g;
+  return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+  const regex = /[0-9]e[0-9]/;
 }
