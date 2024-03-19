9c2209598fe8a35ea3f1e8afe703f2a3e129f85f
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-18 21:05:50 -0300

    upadate function

--- 03/script.js
+++ 03/script.js
@@ -86,6 +86,14 @@ let userData = {
     songCurrentTime: 0,
   };
 
+  const playSong = (id) => {
+    const song = userData?.songs.find((song) => song.id === id);
+  
+  };
+
+
+
+
   const renderSongs = (array) => {
     const songsHTML = array
     .map((song)=> {
