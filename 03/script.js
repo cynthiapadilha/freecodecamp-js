2905be6744aa36d9a112ea437f23da96254dc8a2
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-20 05:13:23 -0300

    add event listener

--- 03/script.js
+++ 03/script.js
@@ -139,6 +139,8 @@ playButton.addEventListener("click", () => {
   }
 });
 
+pauseButton.addEventListener("click",pauseSong)
+
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
     if (a.title < b.title) {
