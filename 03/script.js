dba03b13385414c1b4d41d1619e504481f17a5b6
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-20 05:44:31 -0300

    update functions

--- 03/script.js
+++ 03/script.js
@@ -103,7 +103,7 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
-
+  
   playButton.classList.remove("playing");
   audio.pause();
 };
@@ -113,9 +113,24 @@ const playNextSong = () => {
     playSong(userData?.songs[0].id);
   } else {
     const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
 
+    playSong(nextSong.id);
   }
 };
+
+const playPreviousSong = () => {
+   if (userData?.currentSong === null) return;
+   else {
+    const currentSongIndex = getCurrentSongIndex();
+    const previousSong = userData?.songs[currentSongIndex - 1];
+
+    playSong(previousSong.id);
+   }
+};
+const highlightCurrentSong = () => {
+  
+}
 
 const renderSongs = (array) => {
   const songsHTML = array
@@ -150,6 +165,10 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click",  pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
+
+previousButton.addEventListener("click", playPreviousSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a,b) => {
