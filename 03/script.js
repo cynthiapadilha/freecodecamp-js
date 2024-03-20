1c503115e6c7d8763dcdad5762780f3319871deb
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-20 05:01:49 -0300

    update funcions

--- 03/script.js
+++ 03/script.js
@@ -85,7 +85,10 @@ let userData = {
   songCurrentTime: 0,
 };
 
-
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
 
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
