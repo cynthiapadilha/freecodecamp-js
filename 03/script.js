121bfa4e252b268448e745a3562121d258e69192
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-25 05:28:26 -0300

    finished work

--- 03/script.js
+++ 03/script.js
@@ -259,7 +259,10 @@ audio.addEventListener("ended", () => {
     } else {
       userData.currentSong = null;
       userData.songCurrentTime = 0;  
-
+pauseSong()
+setPlayerDisplay()
+highlightCurrentSong()
+setPlayButtonAccessibleText()
 
     }
 });
