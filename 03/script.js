dd1f1677aa2d9c2fd7502886f58ab16a7220dc27
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-25 05:13:53 -0300

    update function

--- 03/script.js
+++ 03/script.js
@@ -162,6 +162,8 @@ const deleteSong = (id) => {
     const resetText = document.createTextNode("Reset Playlist");
     resetButton.id = "reset";
     resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
   }
 
 };
