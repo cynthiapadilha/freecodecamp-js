d0947e6c7433c732964200aabec341305f7ce331
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-26 05:30:26 -0300

    update switch

--- 04/script.js
+++ 04/script.js
@@ -16,6 +16,8 @@ dateOptionsSelectElement.addEventListener("change", () => {
   switch (dateOptionsSelectElement.value) {
     case "yyyy-mm-dd":
       currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse().join("-")
 
   }
 });
