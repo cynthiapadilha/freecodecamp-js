0efc38fe6ea2201433cf10bbb1b2e885aa014337
Cynthia Padilha <cynthiarfsilva@gmail.com>
2024-03-26 05:34:41 -0300

    finished project

--- 04/script.js
+++ 04/script.js
@@ -17,7 +17,13 @@ dateOptionsSelectElement.addEventListener("change", () => {
     case "yyyy-mm-dd":
       currentDateParagraph.textContent = formattedDate
         .split("-")
-        .reverse().join("-")
-
+        .reverse()
+        .join("-");
+      break;
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+    default:
+    currentDateParagraph.textContent = formattedDate
   }
 });
